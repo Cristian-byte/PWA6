49e7d181000f9ca8fe3ee45445d81a2bd16618e9
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-13 18:57:14 -0500

    Cache dinamico y limitarlo

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
-const CACHE_NAME = 'cache-1';
+//const CACHE_NAME = 'cache-1';
+//const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+const CACHE_DYNAMIC_LIMIT = 50;
 
+function limpiarCache( cacheName, numeroItems ) {
+    cache.opens( cacheName ).then (cache => {
+        return cache.keys().then( keys => {
+            if (keys.length > numeroItems) {
+                cache.delete( keys[0] ).then( limpiarCache(cacheName, numeroItems) );
+            }
+        });
+    });
+}
 
 self.addEventListener('fetch', event => {
 
@@ -44,7 +58,8 @@ self.addEventListener('fetch', event => {
 });
 
     //const cacheProm = caches.open('cache-1')
-    const cacheProm = caches.open(CACHE_NAME)
+    //const cacheProm = caches.open(CACHE_NAME)
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
         .then(cache => {
 
             return cache.addAll([
@@ -53,12 +68,15 @@ self.addEventListener('fetch', event => {
                 '/css/style.css',
                 '/img/main.jpg',
                 '/js/app.js',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+                //'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
             ]);
 
         });
 
-    e.waitUntil(cacheProm);
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 
 
 /*self.addEventListener('fetch', e =>{
@@ -78,12 +96,15 @@ self.addEventListener('fetch', e => {
 
             //No existe el archivo
             //tengo que ir a la web
+            console.log('No existe', e.request.url);
 
             return fetch(e.request).then(newResp => {
 
-                caches.open(CACHE_NAME)
+                //caches.open(CACHE_NAME)
+                caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
+                        limpiarCache( CACHE_DYNAMIC_NAME, 5);
                     });
 
                 return newResp.clone();
