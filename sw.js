bc5e12371486f29c3d12caf4f485b573944def4c
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-13 17:41:59 -0500

    Archivos iniciales del proyecto

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
 
+self.addEventListener('fetch', event => {
+
+    const offlineResp = new Response(`
+    
+        ¡Bienvenido a mi Página Web!
+
+        Discupla, para poder usarla necesitas conexion a internet
+    
+    `);
 
 
+
+    const resp = fetch(event.request)
+        .catch(() => offlineResp);
+
+
+    event.respondWith(resp);
+
+});
